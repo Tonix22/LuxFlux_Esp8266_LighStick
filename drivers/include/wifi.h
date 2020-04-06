58a850967b344e19f3c3a8c9b6c9ff6e6be22bd1
Tonix <emiliotonix@gmail.com>
2020-04-05 20:55:41 -0500

    wifi working for AP and sta

---
+++ drivers/include/wifi.h
@@ -0,0 +1,5 @@
+#ifndef WIFI_H
+#define WIFI_H
+void wifi_init_sta(void);
+void wifi_init_softap(void);
+#endif
--- drivers/wifi.c
+++ drivers/wifi.c
@@ -21,11 +21,16 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-/* The examples use WiFi configuration that you can set via project configuration menu
+static const char *TAG = "wifi Driver";
 
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
+//ACESS POINT
+#define AP_SSID      "LuxFlux"
+#define AP_PASS      "HeliosGriego"
+#define MAX_STA_CONN       4
+
+
+
+//STATION
 #define EXAMPLE_ESP_WIFI_SSID      "IZZI-99CD"
 #define EXAMPLE_ESP_WIFI_PASS      "704FB81799CD"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  10
@@ -38,8 +43,6 @@ static EventGroupHandle_t s_wifi_event_group;
  * - we failed to connect after the maximum amount of retries */
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
-
-static const char *TAG = "wifi station";
 
 static int s_retry_num = 0;
 
@@ -117,4 +120,54 @@ void wifi_init_sta(void)
     ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
     ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
     vEventGroupDelete(s_wifi_event_group);
+}
+
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                    int32_t event_id, void* event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+}
+
+void wifi_init_softap(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    tcpip_adapter_init();//has a flag to avoid doble init
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = AP_SSID,
+            .password = AP_PASS,
+            .ssid_len = strlen(AP_SSID),
+            .channel  = 6,
+            .authmode = WIFI_AUTH_WPA_PSK,
+            .ssid_hidden = 0,
+            .max_connection = MAX_STA_CONN
+        },
+    };
+    if (strlen(AP_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+        printf("OPEN\r\n");
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_softap finished. AP_SSID:%s password:%s",
+             AP_SSID, AP_PASS);
 }
