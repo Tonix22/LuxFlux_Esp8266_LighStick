3054d716a96eb030d86c7574c1f812546338cc48
Tonix <etonix@a2etechnologies.com>
2020-06-04 16:14:29 -0500

    Rainbow for 8 leds corrected

--- drivers/neopixel.c
+++ drivers/neopixel.c
@@ -94,7 +94,6 @@ void Color_Frame(uint8_t R,uint8_t G, uint8_t B)
     uint32_t bit_value = 0;
 
     //printf("color frame: %06X\r\n",cluster);
-    taskENTER_CRITICAL();
 
     interpacket_reset();
     for(uint8_t i = 23; i != 255; i--)
@@ -110,42 +109,63 @@ void Color_Frame(uint8_t R,uint8_t G, uint8_t B)
        }
     }
      //printf("color mask: %06X\r\n",bit_value);
-   taskEXIT_CRITICAL();
+   
 }
 void Pixel_rainbow(void)
 {
     int i=0;
     int j=0;
     int k=0;
+    int pixels = 7;
+    int led;
+    int delay = 10;
     for(i=0;i<255;i++)
     {
-        vTaskDelay(10/ portTICK_RATE_MS);
-        Color_Frame(i,j,k);
+        vTaskDelay(delay/ portTICK_RATE_MS);
+        for(led = 0; led < pixels ;led++)
+        {
+            Color_Frame(i,j,k);
+        }
     }
     for(j=0;j<255;j++)
     {
-        vTaskDelay(10/ portTICK_RATE_MS);
-        Color_Frame(i,j,k);
+        vTaskDelay(delay/ portTICK_RATE_MS);
+        for(led = 0; led < pixels ;led++)
+        {
+            Color_Frame(i,j,k);
+        }
     }
     for(i=255;i!=0;i--)
     {
-        vTaskDelay(10/ portTICK_RATE_MS);
-        Color_Frame(i,j,k);
+        vTaskDelay(delay/ portTICK_RATE_MS);
+        for(led = 0; led < pixels ;led++)
+        {
+            Color_Frame(i,j,k);
+        }
     }
     for(k=0;k<255;k++)
     {
-        vTaskDelay(10/ portTICK_RATE_MS);
-        Color_Frame(i,j,k);
+        vTaskDelay(delay/ portTICK_RATE_MS);
+        for(led = 0; led < pixels ;led++)
+        {
+            Color_Frame(i,j,k);
+        }
     }
     for(j=255;j!=0;j--)
     {
-        vTaskDelay(10/ portTICK_RATE_MS);
-        Color_Frame(i,j,k);
+        vTaskDelay(delay/ portTICK_RATE_MS);
+        for(led = 0; led < pixels ;led++)
+        {
+            Color_Frame(i,j,k);
+        }
     }
 
     for(k=255;k!=0;k--)
     {
-        vTaskDelay(10/ portTICK_RATE_MS);
-        Color_Frame(i,j,k);
+        vTaskDelay(delay/ portTICK_RATE_MS);
+        for(led = 0; led < pixels ;led++)
+        {
+            Color_Frame(i,j,k);
+        }
     }
 }
