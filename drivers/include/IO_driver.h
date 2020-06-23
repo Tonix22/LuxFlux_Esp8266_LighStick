e27bfcf6cdc75973d62527627bbede3e0af56250
Tonix <etonix@a2etechnologies.com>
2020-06-22 22:09:00 -0500

    PCB and Ifdef pinout

--- drivers/include/IO_driver.h
+++ drivers/include/IO_driver.h
@@ -3,9 +3,18 @@
 
 #include "driver/gpio.h"
 #define GPIO_DO 16
+
+#define DEVELOP
+//#define RELEASE
+#ifdef DEVELOP
 #define GPIO_D1 5
+#endif
+#ifdef RELEASE
+#define GPIO_D1 10
+#endif
+
 #define GPIO_D3 0
-#define GPIO_OUT_CONFIG (1ULL << GPIO_DO) | (1ULL << GPIO_D1)
+#define GPIO_OUT_CONFIG (1ULL << GPIO_D1)
 #define GPIO_IN_CONFIG (1UL << GPIO_D3)
 void Output_LED_config(void);
 void input_IO_config(void);
