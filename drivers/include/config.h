1c370f75682477b99e817d0ee9e9d34d34d60a2b
Ruth <ruthsolisvelasco@outlook.com>
2020-07-30 20:59:17 -0500

    Ruth udp a tcp

--- drivers/include/config.h
+++ drivers/include/config.h
@@ -5,6 +5,7 @@
 
 #define RIGHT_STICK (0)
 #define LEFT_STICK  (1)
+#define WIFI_HOME   (0)
 
 #define DEVELOMENT  (1) //GPIO adapat to DEV board
 #define RELEASE     (0) // change on GPIO definition
@@ -17,23 +18,41 @@
 #define MAX_STA_CONN       4
 
 //STATION CONFIG
-#define ESP_WIFI_SSID      "Tonix"
-#define ESP_WIFI_PASS      "typewriter"
+#if WIFI_HOME
+#define ESP_WIFI_SSID      "DEPTO_201"
+#define ESP_WIFI_PASS      "sxk!0363"
+#endif
+
 #define ESP_MAXIMUM_RETRY  10
 #define AUTOCONNECT (0)
+
 
 //This defines changes depending on upper defines
 #if !RIGHT_STICK && !LEFT_STICK
     #error "MUST SELECT A LEDSTICK"
 #endif
 
+#define LEFT_STICK_IP "192.168.4.1"
+#define RIGHT_STICK_IP "192.168.5.1"
+#define PORT 3333
+
 #if RIGHT_STICK
     #define AP_SSID      "LuxFLUX_RIGHT"
     #define CHANGE_DCHP  // avoid IP colision
+    #define ESP_WIFI_SSID      "LuxFLUX_LEFT"
+    #define ESP_WIFI_PASS      "DiaboloSticks"
+
+    //TCP
+    #define HOST_IP_ADDR LEFT_STICK_IP
+    
+
 #endif
 
 #if LEFT_STICK
     #define AP_SSID      "LuxFLUX_LEFT"
+        //TCP
+    #define HOST_IP_ADDR RIGHT_STICK_IP
+    
 #endif
 
 
