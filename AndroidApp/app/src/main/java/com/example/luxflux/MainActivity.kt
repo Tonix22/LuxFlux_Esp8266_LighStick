c47d8a7efb43ace6108fdc9d5920bf68cbeff471
emiliotonix@gmail.com <emiliotonix@gmail.com>
2021-09-30 00:19:42 -0600

    message shown

--- AndroidApp/app/src/main/java/com/example/luxflux/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/example/luxflux/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.luxflux
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Thread{ server() }.start()
+        //server()
+        //
+    }
+    fun server() {
+        val server = ServerSocket(3333)
+        val client = server.accept()
+        //val output = PrintWriter(client.getOutputStream(), true)
+        //val input = BufferedReader(InputStreamReader(client.inputStream))
+        runOnUiThread { Toast.makeText(applicationContext, "Hey! I'm working", Toast.LENGTH_SHORT).show()}
     }
 }
